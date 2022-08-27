7303fa67edee6c6c34e7d075cbc3ccfafeb7afef
kevin-421 <88993079+kevin-421@users.noreply.github.com>
2022-08-27 10:02:37 -0500

    added default browser behavior and added submit event

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,12 @@
-var buttonE1 = document.querySelector("#save-task");
+var formE1 = document.querySelector("#task-form");
 
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
 
 var createTaskHandler = function() {
+
+    event.preventDefault();
+
     var listItemE1 = document.createElement("li");
     
     listItemE1.className = "task-item";
@@ -13,5 +16,5 @@ var createTaskHandler = function() {
     tasksToDoE1.appendChild(listItemE1);
 };
 
-buttonE1.addEventListener('click', createTaskHandler);
+formE1.addEventListener('submit' , createTaskHandler);
     
